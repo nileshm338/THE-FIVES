666f6de9dc26ed07f6336b9a2d29aa3d82df94e3
Nilesh Mishra <100040955+nileshm338@users.noreply.github.com>
2022-11-14 20:22:43 +0530

    Delete Hotel_Management_System_Project directory

--- Hotel_Management_System_Project/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class HotelManagementSystemProjectConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Hotel_Management_System_Project'
